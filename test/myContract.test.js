6386a1d82c0e8aaf86ecc5afa951282e9977eb64
xwr <carlospina997@gmail.com>
2021-06-02 16:29:53 +0000

    npm install web3

--- test/myContract.test.js
+++ test/myContract.test.js
@@ -1,3 +1,4 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
-const web3 = require('web3')                // Consultar ether.js (más ligero)
+// Web3 en mayúscula porque es una clase de JS
+const Web3 = require('web3')                // Consultar ether.js (más ligero)
