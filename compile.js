c9d516c48739e0aed668332447587235af8aab9d
xwr <carlospina997@gmail.com>
2021-06-02 16:21:57 +0000

    20210601

--- compile.js
+++ compile.js
@@ -4,3 +4,23 @@ const solc = require('solc');
 
 const storageName = path.resolve(__dirname, 'contracts', 'storageName.sol');
 const source = fsreadFileSync(storageName, 'utf8');
+
+const input = {
+    language: 'Solidity',
+    sources: {
+        'storageName.sol': {
+            content: source
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*']
+            }
+        }
+    }
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+module.export = output.contracts['storageName.sol'].StorageName;
